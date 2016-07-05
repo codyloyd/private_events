9006d7a8b9ee580dec965f61955f6ca5a129e439
Cody Loyd <codyloyd@gmail.com>
2016-07-05 15:11:27 -0500

    functional user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      flash[:success] = "Account Created"
+      redirect_to root_url
+    else
+      flash.now[:error] = "oops, something broke (try again)"
+      render 'new'
+    end
   end
 
   def show
